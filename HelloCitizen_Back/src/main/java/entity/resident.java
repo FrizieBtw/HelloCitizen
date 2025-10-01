6919578c3b9581ee43c3e8c2a0c3cb1ac70641c2
Nuno-Moreira-Ribeiro <nuno-08@outlook.fr>
2025-10-01 19:31:23 +0200

    Creation of the entity resident

---
+++ HelloCitizen_Back/src/main/java/entity/resident.java
@@ -0,0 +1,115 @@
+package entity;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+public class resident {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Temporal(TemporalType.DATE)
+    private Date birthday;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String number;
+
+    private String address;
+
+    @Temporal(TemporalType.DATE)
+    private Date arrivalDate;
+
+    @Temporal(TemporalType.DATE)
+    private Date notificationDate;
+
+    public resident(Long id) {
+        this.id = id;
+    }
+
+    public resident() {
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Date getArrivalDate() {
+        return arrivalDate;
+    }
+
+    public void setArrivalDate(Date arrivalDate) {
+        this.arrivalDate = arrivalDate;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public Date getNotificationDate() {
+        return notificationDate;
+    }
+
+    public void setNotificationDate(Date notificationDate) {
+        this.notificationDate = notificationDate;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+}
