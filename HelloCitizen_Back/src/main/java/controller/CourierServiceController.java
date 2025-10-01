40150274d617c25873ce9dddd897344a428afb73
Amaral Paulo <frizie08@gmail.com>
2025-10-01 21:53:16 +0200

    Correction courier endpoints

--- HelloCitizen_Back/src/main/java/controller/CourierServiceController.java
+++ HelloCitizen_Back/src/main/java/controller/CourierServiceController.java
@@ -2,6 +2,7 @@ package controller;
 
 import entity.Resident;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,13 +10,13 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/courier")
 class CourierServiceController {
 
-    @PostMapping
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> sendEmail(@RequestBody Resident resident) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(String.format("Email sended to : %s", resident.getEmail()));
+        return ResponseEntity.status(HttpStatus.CREATED).body(String.format("Email sent to : %s", resident.getEmail()));
     }
 
-    @PostMapping
+    @PostMapping(consumes = MediaType.TEXT_PLAIN_VALUE)
     public ResponseEntity<String> sendEmail(@RequestBody String email) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(String.format("Email sended to : %s", email));
+        return ResponseEntity.status(HttpStatus.CREATED).body(String.format("Email sent to : %s", email));
     }
 }
