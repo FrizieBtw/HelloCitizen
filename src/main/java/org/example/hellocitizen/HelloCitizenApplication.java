de0d2459e07e0d646b63f8ba24cad678bdb1dcf0
Amaral Paulo <frizie08@gmail.com>
2025-10-01 15:27:22 +0200

    Project base

---
+++ src/main/java/org/example/hellocitizen/HelloCitizenApplication.java
@@ -0,0 +1,13 @@
+package org.example.hellocitizen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloCitizenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloCitizenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/hellocitizen/HelloCitizenApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hellocitizen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloCitizenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
