5e70884c5596978f768068b3ac29fc8fe5326c91
Amaral Paulo <frizie08@gmail.com>
2025-10-03 17:20:39 +0200

    Route for get attribution by id

--- HelloCitizen_Back/src/main/java/controller/AttributionController.java
+++ HelloCitizen_Back/src/main/java/controller/AttributionController.java
@@ -1,15 +1,30 @@
 package controller;
 
+import entity.Attribution;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import service.AttributionService;
 
 @RestController
-@RequestMapping("/attribution")
+@RequestMapping("/api/attribution")
 class AttributionController {
 
     private final AttributionService attributionService;
 
     AttributionController(AttributionService attributionService) {
         this.attributionService = attributionService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Attribution> getResidentById(@PathVariable Long id) {
+        Attribution attribution = attributionService.findById(id);
+        if (attribution != null) {
+            return ResponseEntity.ok(attribution);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
--- HelloCitizen_Back/src/main/java/service/AttributionService.java
+++ HelloCitizen_Back/src/main/java/service/AttributionService.java
@@ -1,8 +1,21 @@
 package service;
 
+import dao.AttributionDao;
+import entity.Attribution;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-class AttributionService {
+public class AttributionService {
 
+    private final AttributionDao attributionDao;
+
+    @Autowired
+    public AttributionService(AttributionDao attributionDao) {
+        this.attributionDao = attributionDao;
+    }
+
+    public Attribution findById(Long id) {
+        return attributionDao.findById(id);
+    }
 }
